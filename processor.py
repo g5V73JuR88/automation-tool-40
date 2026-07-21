26703846722fffba69307cd8770e019c31070e48
g5V73JuR88 <lephong.01121969@gmail.com>
2026-07-21 15:10:10 +0300

    update implement data processing functionality

--- processor.py
+++ processor.py
@@ -1,29 +1,33 @@
 import json
 
-def read_json(file_path):
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-    return data
+class DataProcessor:
+    def __init__(self, data):
+        self.data = data
 
+    def process(self):
+        self.cleanup()
+        self.reorganize()
 
-def write_json(file_path, data):
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=4)
+    def cleanup(self):
+        self.data = [item for item in self.data if self._is_valid(item)]
 
+    def reorganize(self):
+        self.data.sort(key=lambda x: x['name'])
 
-def merge_data(source_data, new_data):
-    if isinstance(source_data, dict) and isinstance(new_data, dict):
-        source_data.update(new_data)
-    return source_data
+    def _is_valid(self, item):
+        return isinstance(item, dict) and 'name' in item
 
-
-def filter_data(data, keys):
-    return {key: data[key] for key in keys if key in data}
-
+    def to_json(self):
+        return json.dumps(self.data)
 
 # Example usage:
-# if __name__ == '__main__':
-#     data = read_json('input.json')
-#     new_data = {'key': 'value'}
-#     merged_data = merge_data(data, new_data)
-#     write_json('output.json', merged_data)
+if __name__ == '__main__':
+    sample_data = [
+        {'name': 'Alice', 'age': 30},
+        {'name': 'Bob', 'age': 25},
+        {'age': 20},
+        {'name': 'Eve', 'age': 22}
+    ]
+    processor = DataProcessor(sample_data)
+    processor.process()
+    print(processor.to_json())
