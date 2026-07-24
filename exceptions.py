e12d2796ee38da2947fd6ae7020ab49009f2da23
g5V73JuR88 <lephong.01121969@gmail.com>
2026-07-24 15:09:11 +0300

    implement custom exception classes

--- exceptions.py
+++ exceptions.py
@@ -5,15 +5,18 @@ class ValidationError(CustomError):
     def __init__(self, message):
         super().__init__(message)
 
-class DataProcessingError(CustomError):
-    def __init__(self, message):
-        super().__init__(message)
-
-class ConfigurationError(CustomError):
-    def __init__(self, message):
-        super().__init__(message)
-
 class NotFoundError(CustomError):
     def __init__(self, resource):
+        self.resource = resource
         message = f'{resource} not found'
+        super().__init__(message)
+
+class PermissionDeniedError(CustomError):
+    def __init__(self, action):
+        self.action = action
+        message = f'Permission denied for action: {action}'
+        super().__init__(message)
+
+class DatabaseError(CustomError):
+    def __init__(self, message):
         super().__init__(message)
