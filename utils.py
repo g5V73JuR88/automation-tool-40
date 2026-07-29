3ac5458359d0d39f750108af251dde73dfd32db7
g5V73JuR88 <lephong.01121969@gmail.com>
2026-07-29 15:09:10 +0300

    fix json handling utility functions

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 import json
 
-def read_json_file(file_path):
+def load_json(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
 
-def write_json_file(file_path, data):
+def save_json(data, file_path):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
 
@@ -16,9 +16,9 @@ def merge_dicts(dict1, dict2):
     return merged
 
 
-def filter_dict_by_keys(data_dict, keys):
-    return {k: data_dict[k] for k in keys if k in data_dict}
+def filter_dict(data, keys):
+    return {key: data[key] for key in keys if key in data}
 
 
-def pretty_print_json(data):
-    print(json.dumps(data, indent=4))
+def get_keys(data):
+    return list(data.keys())
