ce327f107e80c9dbd4c5424115b368c709b76260
g5V73JuR88 <lephong.01121969@gmail.com>
2026-08-06 15:28:00 +0300

    fix logger setup with rotation

--- config.py
+++ config.py
@@ -1,22 +1,12 @@
-import json
-import os
+import logging
+from logging.handlers import RotatingFileHandler
 
-class ConfigLoader:
-    def __init__(self, default_config_path='default_config.json', user_config_path='user_config.json'):
-        self.default_config = self.load_config(default_config_path)
-        self.user_config = self.load_config(user_config_path)
-        self.combined_config = self.merge_configs(self.default_config, self.user_config)
+def setup_logger(name, log_file, level=logging.INFO):
+    handler = RotatingFileHandler(log_file, maxBytes=1024*1024*5, backupCount=3)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    return logger
 
-    def load_config(self, file_path):
-        if not os.path.isfile(file_path):
-            return {}
-        with open(file_path, 'r') as file:
-            return json.load(file)
-
-    def merge_configs(self, default_config, user_config):
-        combined = default_config.copy()
-        combined.update(user_config)
-        return combined
-
-    def get_config(self):
-        return self.combined_config
+logger = setup_logger('app_logger', 'app.log')
