95ad40e4ad59b4cf703cb3bd4e8b47d9261308bf
g5V73JuR88 <lephong.01121969@gmail.com>
2026-08-15 03:14:13 +0300

    update performance optimization in logger

--- logger.py
+++ logger.py
@@ -1,15 +1,32 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='app.log', max_bytes=10*1024*1024, backup_count=5):
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.set_up_logging()
 
-if __name__ == '__main__':
-    log = setup_logger()
-    log.info('Logger setup complete.')
+    def set_up_logging(self):
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+    def warning(self, msg):
+        self.logger.warning(msg)
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+    @classmethod
+    def disable_logging(cls):
+        logging.disable(logging.CRITICAL)
