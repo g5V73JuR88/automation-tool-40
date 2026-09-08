b52f9375b995f536764e9a6afd91c967ddf7459d
g5V73JuR88 <lephong.01121969@gmail.com>
2026-09-08 03:05:20 +0300

    update log rotation setup for tool

--- logger.py
+++ logger.py
@@ -1,27 +1,29 @@
 import logging
+from logging.handlers import RotatingFileHandler
+import os
 
-class Logger:
-    def __init__(self, name)
-        self.logger = logging.getLogger(name)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+LOG_FILE = "automation-tool-40.log"
+MAX_BYTES = 5 * 1024 * 1024
+BACKUP_COUNT = 3
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        handler = RotatingFileHandler(
+            LOG_FILE, 
+            maxBytes=MAX_BYTES, 
+            backup_count=BACKUP_COUNT
+        )
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
         handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
-        self.logger.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
 
-    def debug(self, message):
-        self.logger.debug(message)
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
 
-    def info(self, message):
-        self.logger.info(message)
-
-    def warning(self, message):
-        self.logger.warning(message)
-
-    def error(self, message):
-        self.logger.error(message)
-
-    def critical(self, message):
-        self.logger.critical(message)
-
-logger = Logger('AutomationTool')
+    return logger
